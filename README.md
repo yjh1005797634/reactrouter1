4ffae2aa707bdebf384da679f5bdb985823e66c9
fyz <fyz97634@163.com>
2018-09-03 11:05:13 +0800

    1.安装路由 2.跑通路由

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 
 1.安装路由
+  npm install react-router-dom -S
+
+2.跑通路由 跑起来就好了
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
+/**
+ * Created by apple on 18/9/3.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+import {
+  HashRouter as Router, //容器
+  Route  // 一条路由
+
+} from 'react-router-dom';
+
+function Home() {
+    return <h1>Home</h1>
+}
+
+function User() {
+    return <h1>User</h1>
+}
+
+function Profile() {
+    return <h1>Profile</h1>
+}
+
+
+
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route path="/home" component={Home}></Route>
+            <Route path="/User" component={User}></Route>
+            <Route path="/Profile" component={Profile}></Route>
+        </div>
+    </Router>,document.querySelector('#root')
+)
